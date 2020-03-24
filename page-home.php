58704e57ee2f9b0aa5166309008af553e4a2d523
Mario Germán Reyes C <mager19@gmail.com>
2020-03-24 15:47:02 -0500

    progress home areas.

--- page-home.php
+++ page-home.php
@@ -12,19 +12,21 @@
 
 get_header(); ?>
 
-<div class="container">
+
+<div class="container padding-content textiles-area">
     <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-4">
             <?php $image = get_field('textiles_cm_image');
-            echo wp_get_attachment_image($image['id'], array(436, 586));
+            echo wp_get_attachment_image($image['id'], $size = 'full');
             ?>
         </div>
-        <div class="col-lg-6">
-            <?php the_field('textiles_cm_content') ?>
+        <div class="col-lg-8 d-flex align-items-center">
+            <div class="content">
+                <?php the_field('textiles_cm_content') ?>
+            </div>
         </div>
     </div>
 </div>
-
 
 <?php
 get_footer();
