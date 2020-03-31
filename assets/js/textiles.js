7b7ac65b3e4fd32137ba7e14a2ed097c0fed2572
Mario Germán Reyes C <mager19@gmail.com>
2020-03-30 21:26:00 -0500

    pin fix

--- assets/js/textiles.js
+++ assets/js/textiles.js
@@ -449,7 +449,8 @@
         var marker = new google.maps.Marker({
             position: latlng,
             map: map,
-            icon: '../wp-content/themes/storefront-child-theme/assets/img/pin2.png',
+            icon: '../wp-content/themes/themeStoreChild/assets/img/pin2.png',
+
         });
 
         // add to array
